833500f775d9d613bdf5afe528403e5a1fa310df
shota-uchiyama <s.uchiyama25@gmail.com>
2020-04-23 19:17:45 +0900

    setting js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,11 +3,11 @@ $(function(){
    if ( message.image ) {
      var html =
       `<div class="message" data-message-id=${message.id}>
-         <div class="upper-message">
-           <div class="upper-message__user-name">
+         <div class="message_upper-info">
+           <div class="message_upper-info_taker">
              ${message.user_name}
            </div>
-           <div class="upper-message__date">
+           <div class="message_upper-info_date">
              ${message.created_at}
            </div>
          </div>
@@ -22,11 +22,11 @@ $(function(){
    } else {
      var html =
       `<div class="message" data-message-id=${message.id}>
-         <div class="upper-message">
-           <div class="upper-message__user-name">
+         <div class="message_upper-info">
+           <div class="message_upper-info_taker">
              ${message.user_name}
            </div>
-           <div class="upper-message__date">
+           <div class="message_upper-info_date">
              ${message.created_at}
            </div>
          </div>
@@ -38,6 +38,45 @@ $(function(){
        </div>`
      return html;
    };
+
+  //  if ( message.image ) {
+  //   var html =
+  //    `<div class="message" data-message-id=${message.id}>
+  //       <div class="upper-message">
+  //         <div class="upper-message__user-name">
+  //           ${message.user_name}
+  //         </div>
+  //         <div class="upper-message__date">
+  //           ${message.created_at}
+  //         </div>
+  //       </div>
+  //       <div class="lower-message">
+  //         <p class="lower-message__content">
+  //           ${message.content}
+  //         </p>
+  //       </div>
+  //       <img src=${message.image} >
+  //     </div>`
+  //   return html;
+  // } else {
+  //   var html =
+  //    `<div class="message" data-message-id=${message.id}>
+  //       <div class="upper-message">
+  //         <div class="upper-message__user-name">
+  //           ${message.user_name}
+  //         </div>
+  //         <div class="upper-message__date">
+  //           ${message.created_at}
+  //         </div>
+  //       </div>
+  //       <div class="lower-message">
+  //         <p class="lower-message__content">
+  //           ${message.content}
+  //         </p>
+  //       </div>
+  //     </div>`
+  //   return html;
+  // };
  }
 
   $('#new_message').on('submit', function(e){
